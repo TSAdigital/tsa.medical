c4dec7d47dc71be9695932c22620153a015bef79
TSAdigital <s.tarhanov174@gmail.com>
2022-01-25 23:46:16 +0500

    Добавил card

--- views/action-history/index.php
+++ views/action-history/index.php
@@ -1,50 +1,48 @@
 <?php
 
-use yii\helpers\Html;
 use yii\widgets\ListView;
-
 
 $this->title = 'Последняя активность';
 $this->params['breadcrumbs'] = [['label' => $this->title]];
 ?>
 <div class="container-fluid">
-     <!-- Timelime example  -->
     <div class="row">
         <div class="col-md-12">
-            <?php
-                $tempalte = "
-                    {summary}       
-                    <div class='timeline mt-3 pb-2'>
-                        {items}
-                        <div>
-                            <i class='fas fa-clock bg-gray'></i>
-                        </div>
-                    </div>
-                    {pager}
-                ";
-            ?>
+            <div class="card">
+                <div class="card-body">
+                    <?php
+                        $tempalte = "
+                            {summary}       
+                            <div class='timeline mt-3 pb-2'>
+                                {items}
+                                <div>
+                                    <i class='fas fa-clock bg-gray'></i>
+                                </div>
+                            </div>
+                            {pager}
+                        ";
+                    ?>
 
-            <?= ListView::widget([
-                'dataProvider' => $actionsHistory,
-                'layout' => $tempalte,
-                'itemOptions' => [
-                    'tag' => false,
-                ],
-                'itemView' => function ($model, $key, $index, $widget)
-                {
-                    static $prevDate = null;
-                    $result = $this->render('_list',
-                        ['model'=>$model, 'printDate' => $prevDate != date('d.m.Y', $model->created_at)]);
-                    $prevDate = date('d.m.Y', $model->created_at);
-                    return $result;
-                },
-                'pager' => [
-                    'class' => 'yii\bootstrap4\LinkPager',
-                ],
-            ]); ?>
-
-
+                    <?= ListView::widget([
+                        'dataProvider' => $actionsHistory,
+                        'layout' => $tempalte,
+                        'itemOptions' => [
+                            'tag' => false,
+                        ],
+                        'itemView' => function ($model, $key, $index, $widget)
+                        {
+                            static $prevDate = null;
+                            $result = $this->render('_list',
+                                ['model'=>$model, 'printDate' => $prevDate != date('d.m.Y', $model->created_at)]);
+                            $prevDate = date('d.m.Y', $model->created_at);
+                            return $result;
+                        },
+                        'pager' => [
+                            'class' => 'yii\bootstrap4\LinkPager',
+                        ],
+                    ]); ?>
+                </div>
+            </div>
         </div>
-        <!-- /.col -->
     </div>
 </div>
