8a587f93c9e93a739786b650231ecb9606aa543f
TSAdigital <s.tarhanov174@gmail.com>
2022-01-26 18:42:06 +0500

    Добавил pagination

--- models/DepartmentSearcha.php
+++ models/DepartmentSearcha.php
@@ -4,7 +4,6 @@ namespace app\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\Department;
 
 /**
  * DepartmentSearcha represents the model behind the search form of `app\models\Department`.
@@ -46,6 +45,9 @@ class DepartmentSearcha extends Department
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'pagination' => [
+                'pageSize' => 10,
+            ],
         ]);
 
         $this->load($params);
