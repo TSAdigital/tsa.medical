b8def4786485e687cc8ac8b02f6a2e22ba50a45f
TSAdigital <s.tarhanov174@gmail.com>
2022-01-26 21:07:11 +0500

    Добавил кнопку вернуться

--- views/departments/update.php
+++ views/departments/update.php
@@ -3,10 +3,15 @@
 /* @var $this yii\web\View */
 /* @var $model app\models\Department */
 
+use yii\helpers\Html;
+
 $this->title = 'Редактирование: ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Подразделения', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Редактирование';
+$this->params['buttons'] = [
+    'undo' => Html::a('<i class="far fa-arrow-alt-circle-left text-muted"></i>Вернуться', ['view', 'id' => $model->id], ['class' => 'btn btn-app'])
+];
 ?>
 
 <div class="container-fluid">
