8c1b4b625e5a99c2a544963e49218f5a0dc19939
TSAdigital <s.tarhanov174@gmail.com>
2022-01-26 19:42:14 +0500

    Добавил страницу history

---
+++ views/users/history.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ListView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\User */
+/* @var $actionsHistory yii\data\ActiveDataProvider */
+
+$this->title = $model->username;
+$this->params['breadcrumbs'][] = ['label' => 'Подразделения', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+$this->params['buttons'] = [
+    'history' => Html::a('<i class="fas fa-undo text-info"></i>Назад', ['view', 'id' => $model->id], ['class' => 'btn btn-app'])
+];
+?>
+
+<div class="container-fluid">
+    <div class="card">
+        <div class="card-body mb-0 pb-0">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 mb-0 pb-0">
+
+                    <?php
+                    $tempalte = "
+                                {summary}       
+                                <div class='timeline mt-3 pb-2'>
+                                    {items}
+                                    <div>
+                                        <i class='fas fa-clock bg-gray'></i>
+                                    </div>
+                                </div>
+                                {pager}
+                            ";
+                    ?>
+                    <?= ListView::widget([
+                        'dataProvider' => $actionsHistory,
+                        'layout' => $tempalte,
+                        'itemOptions' => [
+                            'tag' => false,
+                        ],
+                        'itemView' => function ($model)
+                        {
+                            static $prevDate = null;
+                            $result = $this->render('_list_history',
+                                ['model'=>$model, 'printDate' => $prevDate != date('d.m.Y', $model->created_at)]);
+                            $prevDate = date('d.m.Y', $model->created_at);
+                            return $result;
+                        },
+                        'pager' => [
+                            'class' => 'yii\bootstrap4\LinkPager',
+                        ],
+                    ]); ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
