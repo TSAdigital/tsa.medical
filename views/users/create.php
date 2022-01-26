accad07b11a97bb76d3958c1bab5801a904cfa21
TSAdigital <s.tarhanov174@gmail.com>
2022-01-26 21:06:48 +0500

    Добавил кнопку вернуться

--- views/users/create.php
+++ views/users/create.php
@@ -3,9 +3,14 @@
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
 
+use yii\helpers\Html;
+
 $this->title = 'Новый Пользователь';
 $this->params['breadcrumbs'][] = ['label' => 'Пользователи', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+$this->params['buttons'] = [
+    'undo' => Html::a('<i class="far fa-arrow-alt-circle-left text-muted"></i>Вернуться', ['users/index'], ['class' => 'btn btn-app'])
+];
 ?>
 
 <div class="container-fluid">
