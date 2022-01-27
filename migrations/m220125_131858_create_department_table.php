36319773f821f6396e73816571afa2cb6958046a
TSAdigital <s.tarhanov174@gmail.com>
2022-01-27 14:00:04 +0500

    Пока никто не пользуется сервисом поправил миграцию, добавил колонки для адреса

--- migrations/m220125_131858_create_department_table.php
+++ migrations/m220125_131858_create_department_table.php
@@ -15,6 +15,12 @@ class m220125_131858_create_department_table extends Migration
         $this->createTable('{{%department}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull()->unique(),
+            'index' => $this->integer(),
+            'country' => $this->string(),
+            'region' => $this->string(),
+            'city' => $this->string(),
+            'street' => $this->string(),
+            'building' => $this->string(),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
