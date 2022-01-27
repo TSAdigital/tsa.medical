80ccc4406c600e8b0c00a209be4b35a172dafcb0
TSAdigital <s.tarhanov174@gmail.com>
2022-01-27 21:19:12 +0500

    Добавил кнопку сохранить

--- views/departments/update.php
+++ views/departments/update.php
@@ -10,22 +10,11 @@ $this->params['breadcrumbs'][] = ['label' => 'Подразделения', 'url'
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Редактирование';
 $this->params['buttons'] = [
+    'save' => Html::submitButton('<i class="far fa-save text-green"></i>Сохранить', ['class' => 'btn btn-app', 'form'=>"department"]),
     'undo' => Html::a('<i class="far fa-arrow-alt-circle-left text-muted"></i>Вернуться', ['view', 'id' => $model->id], ['class' => 'btn btn-app'])
 ];
 ?>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-
-                    <?= $this->render('_form', [
-                        'model' => $model
-                    ]) ?>
-
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="card">
+    <?= $this->render('_form', ['model' => $model]) ?>
 </div>
