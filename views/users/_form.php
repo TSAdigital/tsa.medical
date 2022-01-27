29ac93fdb0d976a65b9db03a94527077ff618d8f
TSAdigital <s.tarhanov174@gmail.com>
2022-01-27 21:24:06 +0500

    Добавил id для формы

--- views/users/_form.php
+++ views/users/_form.php
@@ -1,6 +1,5 @@
 <?php
 
-use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 
 /* @var $this yii\web\View */
@@ -10,16 +9,12 @@ use yii\bootstrap4\ActiveForm;
 
 <div class="user-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['id' => 'user']); ?>
 
     <?= $form->field($model, 'username')->textInput() ?>
     <?= $form->field($model, 'email')->textInput() ?>
     <?= $form->field($model, 'password')->passwordInput(['autocomplete' => 'new-password']) ?>
     <?= $form->field($model, 'roles')->dropdownList($model->getRolesDropdown(), ['prompt' => 'Выберите роль']) ?>
-
-    <div class="form-group mb-0">
-        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
-    </div>
 
     <?php ActiveForm::end(); ?>
 
