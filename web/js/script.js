4c6caf7dfe01e7668aa3fc376e76bbc88852a513
TSAdigital <s.tarhanov174@gmail.com>
2022-01-28 18:40:06 +0500

    Временный костыль, отображает информацию на вкладке где есть ошибки валидации.

--- web/js/script.js
+++ web/js/script.js
@@ -1,12 +1,16 @@
+const table = function(data) {
+    if ($("#tab"+data).find('.invalid-feedback').text().length <= 0) {
+        $(".tab-"+data).html('');
+    } else {
+        $(".tab-"+data).html('<sup><i class="fas fa-exclamation-circle text-danger"></i></sup>');
+    }
+}
 $(".tab-pane").bind("DOMSubtreeModified", function(){
-    if($("#tab1").find('.invalid-feedback').text().length > 0) {
-        $(".tab-1").html('<sup><i class="fas fa-exclamation-circle text-danger"></i></sup>');
-    }else{
-        $(".tab-1").html('');
-    }
-    if($("#tab2").find('.invalid-feedback').text().length > 0) {
-        $(".tab-2").html('<sup><i class="fas fa-exclamation-circle text-danger"></i></sup>');
-    }else{
-        $(".tab-2").html('');
-    }
+    'use strict';
+    table(1);
+    table(2);
+    table(3);
 });
+table(1);
+table(2);
+table(3);
