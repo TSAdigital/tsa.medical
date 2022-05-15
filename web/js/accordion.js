d39a29a46706e37dd067373dbeb5b3c38ac36f28
TSAdigital <s.tarhanov174@gmail.com>
2022-05-15 23:39:31 +0500

    Роли

--- web/js/accordion.js
+++ web/js/accordion.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-    // Get saved data from sessionStorage
     let selectedCollapse = sessionStorage.getItem('selectedCollapse');
     if(selectedCollapse != null) {
         $('.accordion .collapse').removeClass('show');
         $(selectedCollapse).addClass('show');
     }
-    //To set, which one will be opened
+
     $('.accordion .btn-link').on('click', function(){
         let target = $(this).data('target');
         //Save data to sessionStorage
